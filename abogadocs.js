98c6ae6b4a09895c06db532482240f64f45aeb49
¨Julian <juliangarciam13@hotmail.com>
2023-04-27 18:54:23 -0500

    The function that calls the data from the api was created and a map with 7 properties was made

--- abogadocs.js
+++ abogadocs.js
@@ -1,13 +1,27 @@
-const apiUrl = 'https://cdn.filestackcontent.com/HANDLE';
-
-const documents = async () => {
+const fetchLegalResource = async () => {
+    const url = `http://openlibrary.org/search.json?q=&subject=law`;
     try {
-        const dataDocuments = await fetch("https://www.googleapis.com/drive/v3/files/fileId",
-            { method: 'GET'});
-        console.log(dataDocuments);
+        const dataDocuments = await fetch(url,
+            { method: 'GET' });
+        const jsonDataDocuments = await dataDocuments.json();
+        const mapDocuments = jsonDataDocuments.docs.map((result) => {
+            const coverID = result.cover_i || '';
+            const coverURL = `http://covers.openlibrary.org/b/id/${coverID}-M.jpg`;
+            return {
+                'cover_url': coverURL,
+                'title': result.title,
+                'first_publish_year': result['first_publish_year'],
+                'number_of_pages_median': result['number_of_pages_median'],
+                'key': result.key,
+                'cover_i': result['cover_i'],
+                'edition_count': result['edition_count']
+            }
+        });
+        console.log(mapDocuments);
     } catch (error) {
         console.error(error);
+        console.log(error.message);
     }
 }
 
-documents();
+fetchLegalResource();
