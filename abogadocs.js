b7b89663d6a897c8ba1d90ac5155ea51441955df
¨Julian <juliangarciam13@hotmail.com>
2023-05-01 22:28:55 -0500

    The function of downloading books in pdf was removed

--- abogadocs.js
+++ abogadocs.js
@@ -52,36 +52,3 @@ pdfDownloadLink.addEventListener('click', function (event) {
   event.preventDefault();
   downloadPdf();
 });
-
-// funcion para descargar el libro en formato pdf
-/*const downloadBook = async (key) => {
-  try {
-    // Get the book metadata from the Open Library API
-    const bookUrl = `https://openlibrary.org/books/${key}.json`;
-    const bookResponse = await fetch(bookUrl);
-    const bookData = await bookResponse.json();
-    
-    // Check if the book is available in PDF format
-    if (bookData && bookData.formats && bookData.formats['application/pdf']) {
-      // Get the PDF download link from the book metadata
-      const pdfUrl = bookData.formats['application/pdf'];
-      
-      // Download the PDF
-      const pdfResponse = await fetch(pdfUrl);
-      const pdfBlob = await pdfResponse.blob();
-      const pdfFilename = `${bookData.title}.pdf`;
-      const pdfLink = URL.createObjectURL(pdfBlob);
-      const pdfDownloadLink = document.createElement('a');
-      pdfDownloadLink.href = pdfLink;
-      pdfDownloadLink.download = pdfFilename;
-      pdfDownloadLink.click();
-    } else {
-      console.log('This book is not available in PDF format.');
-    }
-  } catch (error) {
-    console.error(error);
-  }
-}
-
-downloadBook('OL28197656M');
- */
